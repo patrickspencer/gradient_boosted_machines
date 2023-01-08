daa06c115e0d9e66fdd14d1f50648c26a75a2a8b
Patrick Spencer <pkspenc@gmail.com>
2023-01-07 20:46:41 -0500

    Update train.py for new model and data loader

--- train.py
+++ train.py
@@ -1,31 +1,17 @@
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeRegressor
-import pandas as pd
-import numpy as np
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-data = datasets.load_breast_cancer()
+from data import DataLoader
+from model import GradientBoostedMachine
 
-df = pd.DataFrame(data.data, columns=data.feature_names)
 
-X = data.data
-y = data.target
+def train(X, y):
+    """Train method"""
+    clf = GradientBoostedMachine(n_trees=100, learning_rate=0.1, max_depth=3)
+    clf.fit(X, y)
+    return clf
 
-X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-learning_rate = 0.3
-n_trees = 10
-max_depth = 100
-
-F0 = y.mean()
-Fm = F0
-trees = []
-
-for _ in range(n_trees):
-    tree = DecisionTreeRegressor(max_depth=max_depth)
-    tree.fit(X_train, y_train - Fm)
-    Fm += learning_rate * tree.predict(X_train)
-    trees.append(tree)
-
-trees_predict = [t.predict(X_train) for t in trees]
-y_hat = F0 + learning_rate * np.sum(trees_predict, axis=0)
+if __name__ == "__main__":
+    data = DataLoader()
+    train(data.X_train, data.y_train)
