29f03561398d2c1b969cb6e90ebbd8c61c50f5ab
Patrick Spencer <pkspenc@gmail.com>
2023-01-08 19:32:44 -0500

    Update model.py variable names

--- model.py
+++ model.py
@@ -12,7 +12,7 @@ class GradientBoostedMachine:
         self.n_trees = n_trees
         self.learning_rate = learning_rate
         self.max_depth = max_depth
-        self.trees = []
+        self.error_clfs = []
         self.f_zero = 0
 
     def fit(self, X, y) -> None:
@@ -20,12 +20,12 @@ class GradientBoostedMachine:
         self.f_zero = y.mean()
         f_m = self.f_zero
         for _ in range(self.n_trees):
-            tree = DecisionTreeRegressor(max_depth=self.max_depth)
-            tree.fit(X, y - f_m)
-            f_m += self.learning_rate * tree.predict(X)
-            self.trees.append(tree)
+            error_clf = DecisionTreeRegressor(max_depth=self.max_depth)
+            error_clf.fit(X, y - f_m)
+            f_m += self.learning_rate * error_clf.predict(X)
+            self.error_clfs.append(error_clf)
 
     def predict(self, X) -> np.ndarray:
         """Predict method for making predictions"""
-        predictions = [t.predict(X) for t in self.trees]
+        predictions = [t.predict(X) for t in self.error_clfs]
         return self.f_zero + self.learning_rate + np.sum(predictions, axis=0)
