478c9b873dacef011b9fa01c9c0a5d945530df22
Patrick Spencer <pkspenc@gmail.com>
2023-01-09 19:44:14 -0500

    Refactor python scripts to lib/ folder

--- lib/tests.py
+++ lib/tests.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from train import train
-from data import DataLoader
+from lib.data import DataLoader
 from model import GradientBoostedMachine
 
 
--- lib/train.py
+++ lib/train.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from data import DataLoader
+from lib.data import DataLoader
 from model import GradientBoostedMachine
 
 
